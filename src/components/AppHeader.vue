c2942dbf157ef7118e0e37d796fb76cd02f517d5
Thanh Truong <huethanhst@gmail.com>
2024-09-24 14:08:31 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">ỨNG DỤNG QUẢN LÝ DANH BẠ</a>
+        <dic class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                    DANH BẠ
+                <i class="fas fa-address-book"></i>
+                </router-link>
+            </li>
+        </dic>
+    </nav>
+</template>
